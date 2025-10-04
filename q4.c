e249d88c92a2bc9820eded680fea8b0c6a0848de
mayafrncis <maya.ara.francis@gmail.com>
2025-10-04 13:27:24 +0000

    edited 4

--- q4.c
+++ q4.c
@@ -5,21 +5,23 @@ int main() {
 	char** arr = (char **) malloc(3 * sizeof(char *));
 	for (int i = 0; i < 3; i++)
 		*(arr + i) = (char *) malloc(50 * sizeof(char));
-	printf("Enter 3 strings:");
+	printf("Enter 3 strings: ");
         for (int i = 0; i < 3; i++) {
                 scanf("%s", *(arr + i));
         }
 	arr = (char **) realloc(arr, 5 * sizeof(char *));
 	for (int i = 3; i < 5; i++)
                 *(arr + i) = (char *) realloc(*(arr + i), 50 * sizeof(char));
-	printf("Enter 2 more strings:");
+	printf("Enter 2 more strings: ");
 	for (int i = 3; i < 5; i++) {
                 scanf("%s", *(arr + i));
         }
+	printf("All strings: ");
         for (int i = 0; i < 5; i++) {
-                printf("%s\n", *(arr + i));
+                printf("%s ", *(arr + i));
                 free(*(arr + i));
         }
+	printf("\n");
         free(arr);
         arr = NULL;
 
