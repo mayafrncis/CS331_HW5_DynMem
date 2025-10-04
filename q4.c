18872bb5a295741c83584b76c0cbd5dbca699c3c
mayafrncis <maya.ara.francis@gmail.com>
2025-10-04 13:25:09 +0000

    q4

--- q4.c
+++ q4.c
@@ -9,7 +9,14 @@ int main() {
         for (int i = 0; i < 3; i++) {
                 scanf("%s", *(arr + i));
         }
-        for (int i = 0; i < 3; i++) {
+	arr = (char **) realloc(arr, 5 * sizeof(char *));
+	for (int i = 3; i < 5; i++)
+                *(arr + i) = (char *) realloc(*(arr + i), 50 * sizeof(char));
+	printf("Enter 2 more strings:");
+	for (int i = 3; i < 5; i++) {
+                scanf("%s", *(arr + i));
+        }
+        for (int i = 0; i < 5; i++) {
                 printf("%s\n", *(arr + i));
                 free(*(arr + i));
         }
